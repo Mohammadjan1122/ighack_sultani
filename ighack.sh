51acd52dbcd0404c6159a9bc2bb5a7f7b97fdb3a
Mohammadjan1122 <63337310+Mohammadjan1122@users.noreply.github.com>
2020-07-01 18:11:52 +0430

    Update ighack.sh

--- ighack.sh
+++ ighack.sh
@@ -46,13 +46,13 @@ read -p $'\n\e[1;92m[\e[0m\e[1;77m*\e[0m\e[1;92m] Choose an option: \e[0m' optio
 
 
 if [[ $option == 1 || $option == 01 ]]; then
-cd $HOME/ighack_sultani_sultani/main
+cd $HOME/ighack_sultanikin
 bash auto.sh
 elif [[ $option == 2 || $option == 02 ]]; then
-cd $HOME/ighack_sultani_sultani/main
+cd $HOME/ighack_sultani/main
 bash manual.sh
 elif [[ $option == 3 || $option == 03 ]]; then
-cd $HOME/ighack_sultani_sultani/core
+cd $HOME/ighack_sultani/core
 bash about.sh
 elif [[ $option == 4 || $option == 04 ]]; then                                                                                                                
 cd $HOME/ighack_sultani/core
